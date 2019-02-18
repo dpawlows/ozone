fbbdede2e5b40db65f4dd0417dfb1e85fef57eae
Dave Pawlowski <dpawlows80@gmail.com>
2019-02-18 10:58:25 +0100

    updated readoutput

--- srcOutput/readOutput.py
+++ srcOutput/readOutput.py
@@ -6,12 +6,15 @@ def readOutputFile(file):
     for line in f:
         if started:
             data.append([float(item) for item in line.split("\t")])
-        if line[0] == '#':
+        if line[0:4] == '#Alt':
             started = True
             header = line.strip('#').strip("\n").split("\t")
             nvars = len(header)
-
+        if line[0:7] == "#Planet":
+            t = line.split()
+            planetDistance = float(t[1])
     data = array(data)
     f.close()
 
+    header.append("r={}".format(planetDistance))
     return data,header
