bb37e052454f265b51240d4ec0ab138133a6e2ca
Dave Pawlowski <dpawlows80@gmail.com>
2019-04-11 14:18:57 +0200

    added mock imports so autodocs works on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ import sys
 sys.path.insert(0, os.path.abspath('../src/'))
 sys.path.insert(0, os.path.abspath('../input/'))
 
-autodoc_mock_imports = ['astropy']
-autodoc_mock_imports = ['scipy']
+autodoc_mock_imports = ['astropy','scipy']
+
 
 
 # -- Project information -----------------------------------------------------
