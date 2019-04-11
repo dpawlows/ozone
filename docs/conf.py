17967f55f72f9ae98b72deabb62bc0b34198569e
Dave Pawlowski <dpawlows80@gmail.com>
2019-04-11 09:49:41 +0200

    fixed doc config.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 html_theme = 'agogo'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -92,7 +92,7 @@ html_theme = 'agogo'
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ['_static']
-html_style = 'style.css'
+# html_style = 'style.css'
 
 
 # Custom sidebar templates, must be a dictionary that maps document names
